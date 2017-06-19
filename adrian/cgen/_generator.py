01df07dbdd38cc0166c75592c7f8508b5618dc21
Elliptica <elliptica.magnificum@gmail.com>
2017-06-19 20:47:10 +0300

    Add high-level decl translation function without low-level functions' implementations

--- adrian/cgen/_generator.py
+++ adrian/cgen/_generator.py
@@ -6,6 +6,18 @@ from . import _layers
 
 class NodeGenerator(_layers.Layer):
 
+    def _type(self, type_):
+        # TODO: implement
+        # return "int"
+        errors.not_implemented()
+
+    def _expr(self, expr):
+        if isinstance(expr, objects.Val):
+            return self._val(expr)
+        elif isinstance(expr, objects.Var):
+            return self._var(expr)
+        errors.not_implemented()
+
     @_layers.register(objects.Val)
     def _val(self, val):
         if isinstance(val.type_, tuple(map(type, (
@@ -20,6 +32,15 @@ class NodeGenerator(_layers.Layer):
     @_layers.register(objects.Var)
     def _var(self, var):
         return var.name
+
+    @_layers.register(objects.Decl)
+    def _decl(self, decl):
+        return " ".join([
+            self._type(decl.type_),
+            decl.name,
+            "=",
+            "".join([self._expr(decl.expr), ";"])
+        ])
 
     def generate(self, node):
         return self.get_registry()[node](node)
