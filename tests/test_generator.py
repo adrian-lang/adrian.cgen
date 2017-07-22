c5a0a4c6b5b5296c0cf696646e58390b421fc031
Elliptica <elliptica.magnificum@gmail.com>
2017-07-22 11:44:34 +0300

    Add large test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,6 +3,13 @@ import unittest
 from adrian import cgen
 
 
+stdlib_incl = cgen.Include("stdlib.h")
+malloc_func = cgen.CFuncDescr(
+    name="malloc", rettype=cgen.CTypes.ptr(cgen.CTypes.void),
+    args=(cgen.CTypes.size_t, ),
+    includes=[stdlib_incl])
+
+
 class StructTest(unittest.TestCase):
 
     def test_it_generates(self):
@@ -21,11 +28,6 @@ class StructTest(unittest.TestCase):
             "\n".join(list(generator.generate())))
 
     def test_use_in_complex_function(self):
-        stdlib_incl = cgen.Include("stdlib.h")
-        malloc_func = cgen.CFuncDescr(
-            name="malloc", rettype=cgen.CTypes.ptr(cgen.CTypes.void),
-            args=(cgen.CTypes.size_t, ),
-            includes=[stdlib_incl])
         inp = [
             cgen.Func(
                 "initMyStruct", rettype=cgen.CTypes.ptr(cgen.StructType("MyStruct")),
@@ -67,6 +69,51 @@ class StructTest(unittest.TestCase):
         self.assertEqual(
             "\n".join([
                 "#include <stdint.h>",
+                "struct MyStruct* lol() {",
+                "return initMyStruct(23);",
+                "}"
+            ]),
+            "\n".join(list(generator.generate())))
+
+    def test_use_in_large_example(self):
+        inp = [
+            cgen.Struct(
+                "MyStruct", (
+                    cgen.Decl("data", type_=cgen.CTypes.int_fast8), )),
+            cgen.Func(
+                "initMyStruct", rettype=cgen.CTypes.ptr(cgen.StructType("MyStruct")),
+                args=(cgen.Decl("data", type_=cgen.CTypes.int_fast8), ),
+                body=(
+                    cgen.Decl(
+                        "self",
+                        type_=cgen.CTypes.ptr(cgen.StructType("MyStruct")),
+                        expr=malloc_func(cgen.SizeOf(cgen.StructType("MyStruct")))),
+                    cgen.Assignment(
+                        name=cgen.StructElem(
+                            cgen.CTypes.ptr(cgen.Var("self")), "data"),
+                        expr=cgen.Var("data")),
+                    cgen.Return(cgen.Var("self")))),
+            cgen.Func(
+                "lol", rettype=cgen.CTypes.ptr(cgen.StructType("MyStruct")),
+                args=(),
+                body=(
+                    cgen.Return(
+                        cgen.FuncCall("initMyStruct", cgen.Val("23", type_=cgen.CTypes.int_fast8))), ))
+        ]
+        generator = cgen.Generator()
+        generator.add_ast(inp)
+        self.assertEqual(
+            "\n".join([
+                "#include <stdint.h>",
+                "#include <stdlib.h>",
+                "struct MyStruct {",
+                "int_fast8_t data;",
+                "};",
+                "struct MyStruct* initMyStruct(int_fast8_t data) {",
+                "struct MyStruct* self = malloc(sizeof(struct MyStruct));",
+                "self->data = data;",
+                "return self;",
+                "}",
                 "struct MyStruct* lol() {",
                 "return initMyStruct(23);",
                 "}"
