2dab2c1593e9dbf47fb351398fc63cdd7b3a6db8
Pavlo Kapyshin <i@93z.org>
2017-07-24 20:53:06 +0200

    Note that make_main0 expects whole body

--- adrian/cgen/utils.py
+++ adrian/cgen/utils.py
@@ -2,7 +2,12 @@ from adrian.cgen import Func, CTypes
 
 
 def make_main0(*body):
-    """Make 0-argument main: int main(void) {...}."""
+    """Make 0-argument main: int main(void) {...}.
+
+    Note that return is not added automatically: you
+    provide whole body of main (including return statements).
+
+    """
     return Func(
         "main", rettype=CTypes.int,
         args=CTypes.void, body=body)
